61888f6e4bbb6ce9179905f5e6edb583ab5502dc
kang <zhaok365@outlook.com>
2024-03-15 11:08:06 +0800

    远程与本地ui有冲突，且web线程无法停止

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
+import threading
 import tkinter
-import cv2
 import gui
+import tag
 import videoProcess
-
+from remote.remote import create_video_server
 
 
 def main():
     Rec = videoProcess.faceRec(
         r"D:\Special_Tools\Anaconda\envs\OCR\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml")
-
+    app = create_video_server('remote/users.txt', Rec)
+    thread_web = threading.Thread(target=app.run, kwargs={"host": '0.0.0.0'})
+    thread_web.start()
     gui.CameraApp(tkinter.Tk(), "摄像头应用", Rec)
+    tag.running = 0
+    thread_web.join()
 
 
 if __name__ == "__main__":
--- remote/remote.py
+++ remote/remote.py
@@ -1,53 +1,53 @@
-from flask import Flask, render_template, Response, request, redirect, url_for
+# 读取摄像头视频流，使用videoProcess部分处理后展示至web客户端
+from flask import Flask, Response, render_template, request
 import cv2
-import videoProcess
-
-app = Flask(__name__, template_folder='templates')
-
-# 设置用户名和密码
-verify = {}
-with open('users.txt') as file:
-    for line in file:
-        words = line.split(' ')
-        verify[words[0]] = words[1]
+import tag
 
 
-def generate_frames():
-    camera = cv2.VideoCapture(0)  # 0 for built-in webcam
-    while True:
-        success, frame = camera.read()
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+def create_video_server(users_file, rec):
+    app = Flask(__name__, template_folder='templates')
+    # 设置用户名和密码
+    verify = {}
+    with open(users_file) as file:
+        for line in file:
+            words = line.split(' ')
+            verify[words[0]] = words[1]
 
+    def generate_frames():
+        camera = cv2.VideoCapture(0)  # 0 for built-in webcam
+        while tag.running:
+            success, frame = camera.read()
+            frame = rec.recognize(frame)
+            if not success:
+                break
+            else:
+                ret, buffer = cv2.imencode('.jpg', frame)
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-# 验证用户身份
-def check_auth(username, password):
-    return username in verify.keys() and password == verify[username]
+    # 验证用户身份
+    def check_auth(username, password):
+        return username in verify.keys() and password == verify[username]
 
+    # 身份验证失败
+    def unauthorized():
+        return Response('Unauthorized', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
-# 身份验证失败
-def unauthorized():
-    return Response('Unauthorized', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
+    @app.route('/video_feed')
+    def video_feed():
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return unauthorized()
+        return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/video_feed')
-def video_feed():
-    auth = request.authorization
-    if not auth or not check_auth(auth.username, auth.password):
-        return unauthorized()
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return app
 
 
 if __name__ == "__main__":
+    app = create_video_server('users.txt')
     app.run(host='0.0.0.0')
-
